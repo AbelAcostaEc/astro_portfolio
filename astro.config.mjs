0c83eb8b36d864b5ec77a48ef59be7e7907d5fbd
Abel Acosta <61747769+AbelAcostaEc@users.noreply.github.com>
2025-04-03 00:25:50 -0500

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://abelacostaec.github.io',
-    base: 'astro_portfolio',
+  site: 'https://abelacostaec.github.io',
+  base: 'astro_portfolio',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
